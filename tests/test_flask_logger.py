a6bb7b021e8a192bb0792864ef5573114959297b
jessmchung <jchung@theorchard.com>
2016-03-22 12:21:36 -0400

    Add correlation id to flask response headers.

--- tests/test_flask_logger.py
+++ tests/test_flask_logger.py
@@ -8,8 +8,8 @@ import pytest
 import uuid
 
 from sample import app
+from owslogger import constants
 from owslogger import flask_logger
-
 
 
 def test_setting_up_flask_app():
@@ -38,7 +38,7 @@ def test_app_correlation_id_reuse():
 
     correlation_id = str(uuid.uuid1())
     with app.test_request_context(
-            '/hello/', headers={'Correlation-Id': correlation_id}):
+            '/hello/', headers={constants.CORRELATION_ID_HEADER: correlation_id}):
         app.global_correlation_id()
         assert g.correlation_id == correlation_id
 
@@ -53,3 +53,12 @@ def test_logger_creation():
         assert isinstance(g.log, logging.LoggerAdapter)
         assert 'correlation_id' in g.log.extra
         assert g.log.extra.get('correlation_id') == g.correlation_id
+
+
+def test_add_correlation_id_to_response():
+    """Test correlation id is added to response header
+    """
+
+    test_client = app.test_client()
+    response = test_client.get('/hello/')
+    assert response.headers.get(constants.CORRELATION_ID_HEADER)
